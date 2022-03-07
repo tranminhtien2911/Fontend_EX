68c2b5f826516f9e5f30696dca28550d5fd01233
tmtien2911 <tmtien2911@gmail.com>
2022-03-07 17:00:39 +0700

    HTMl DOM

---
+++ htmlDOM.js
@@ -0,0 +1,13 @@
+//HTML DOM
+
+//1. Element: ID, class, tag name, CSS selector, HTML collection
+
+//2. Attribute
+
+//3. Text
+
+var headingNode = document.getElementById('heading');
+console.log(headingNode);
+//Kết quả trả về là nguyên cái thẻ <h1 id="heading">HTML DOM</h1> 
+// Và nguyên cái thẻ thẻ trên là Object, nó mô tả headingNode là 1 cái node
+//---- Để có thể nhìn rõ được thì phải đưa headingNode vào 1 cái mảng 
