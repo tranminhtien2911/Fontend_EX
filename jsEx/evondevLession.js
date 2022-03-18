b8ab6ee20304927a144d719c4f10bffab76699bf
tmtien2911 <tmtien2911@gmail.com>
2022-03-18 17:00:01 +0700

    new evondev

---
+++ jsEx/evondevLession.js
@@ -0,0 +1,39 @@
+//promt, confirm, alert
+// alert("Your website has been hacked");
+
+// prompt("Mời bạn điền tên :))))");
+// Để lấy được giá trị được điền vào của promt, chúng ta đặt cho nó 1 cái biến
+// const yourName = prompt("Mời bạn điền tên :))))");
+// console.log(yourName);
+
+// const isYourMoney = confirm("Day co phai la tien cua ban hay khong ?");
+// console.log(isYourMoney)
+
+//---Bài tập:
+// Câu điều kiện:
+/**
+ * Bài 1:
+ * Nhập vào số tuổi và kiểm tra nếu số tuổi lớn hơn hoặc bằng 18 thì được coi phim rạp
+ * ngược lại thì không được vô coi
+*/
+// var yourAge = prompt("Nhập vào số tuổi của bạn để kiểm duyệt xem phim !!!", "")
+// console.log(typeof yourAge); //kq ra kiểu string nên ta cần Chuyển đổi kiểu
+// if (Number(yourAge) >= 18){
+//     alert("Mời bạn vào rạp phim");
+// }else{
+//     alert("Số tuổi của bạn chưa đủ để xem bộ phim này");
+// }
+/**
+ * Bài 2:
+ * Cho 2 số a và b, tìm ra số lớn hơn
+ */
+const a =  10;
+const b = 5;
+
+if(a>b){
+    alert(`số lớn hơn là số ${a}`);
+}else if(a<b){
+    alert(`Số lớn hơn là số ${b}`);
+}else{
+    alert('Không có số nào lớn hơn số nào cả')
+}
